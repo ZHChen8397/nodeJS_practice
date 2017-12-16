95d7f0ded43458b02fe657ac540c5a0134aeb0bd
ZHChen8397 <zhchen8397@gmail.com>
2017-12-16 17:11:47 +0800

    Sinon mock
    Sinon mark(with arg) & unit test

--- modules/sinon.js
+++ modules/sinon.js
@@ -8,3 +8,11 @@ module.exports.testExistFunction = function(){
   }
   return object 
 }
+
+module.exports.testMock = function(){
+  return 123
+}
+
+module.exports.testMockwithArg = function(name){
+  return 'hell' + name
+}
--- test/sinon.spec.js
+++ test/sinon.spec.js
@@ -9,11 +9,27 @@ describe('sinon test',function(){
     assert.ok(s.called)
   })
 
-  it.only('should using a spy to wrap an existing method',function(){
+  it('should using a spy to wrap an existing method',function(){
     let s = sinon.spy(sinonM,'testExistFunction')
     let obj = sinonM.testExistFunction()
     assert.ok(s.calledOnce)
     assert.equal(s.getCall(0).returnValue.name,'jeff')
+  })
+
+  it('should be mock by sinon mock',function(){
+    let sinonMock = sinon.mock(sinonM)
+    sinonMock.expects('testMock').resolves(456).once()
+    let r =sinonM.testMock()
+    assert.deepEqual(r,Promise.resolve (456) )
+    sinonMock.verify()
+    sinonMock.restore()
+  })
+
+  it.only('should be mock by sinon mock with arg',function(){
+    let sinonMock = sinon.mock(sinonM)
+    sinonMock.expects('testMockwithArg').withArgs('jeff').resolves('seafood noob')
+    let r = sinonM.testMockwithArg('jeff')
+    assert.deepEqual(r,Promise.resolve('seafood noob'))
   })
 })
 
